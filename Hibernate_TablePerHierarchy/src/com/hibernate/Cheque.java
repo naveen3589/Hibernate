b51ce37f0da5ea02f36d89ba8dc6d08a209dacd7
NAVEEN YARRAMSETTY <ysn.naveen@users.noreply.github.com>
2018-11-30 14:27:28 +0530

    Hibernate
    HibernateDemo

---
+++ Hibernate_TablePerHierarchy/src/com/hibernate/Cheque.java
@@ -0,0 +1,16 @@
+package com.hibernate;
+
+public class Cheque extends Payment {
+
+	private String chequeType;
+
+	public String getChequeType() {
+		return chequeType;
+	}
+
+	public void setChequeType(String chequeType) {
+		this.chequeType = chequeType;
+	}
+
+	
+}
